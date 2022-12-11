999c5b5e0c9305339c7806b846d1499011de9fb0
timonforrer <timon.forrer@gmail.com>
2022-12-11 12:57:43 +0100

    Add schema to group Modular Pages

---
+++ studio/schemas/documents/documentGroup.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'documentGroup',
+  title: 'Document group',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'ordering',
+      title: 'Ordering',
+      type: 'number'
+    }
+  ],
+}
