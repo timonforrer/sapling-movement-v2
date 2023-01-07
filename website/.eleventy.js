15ecdc13937d32e05f7d43ba3c7f2b1f03934fd1
timonforrer <timon.forrer@gmail.com>
2023-01-07 18:19:36 +0100

    Add preview functionality and WebC support

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,10 +1,25 @@
+const { EleventyServerlessBundlerPlugin, EleventyRenderPlugin } = require('@11ty/eleventy');
+const WebC = require('@11ty/eleventy-plugin-webc');
 require('dotenv').config();
 
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} config 
  */
 
-module.exports = function() {
+module.exports = function(config) {
+
+  config.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'preview',
+    functionsDir: './netlify/functions',
+    copy: ['src/utils']
+  });
+
+  config.addPlugin(EleventyRenderPlugin);
+
+  config.addPlugin(WebC, {
+    components: 'src/components/*.webc'
+  });
+  
   return {
     dir: {
       data: 'data',
